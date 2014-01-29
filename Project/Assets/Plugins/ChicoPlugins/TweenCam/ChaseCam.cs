b28bf2556653433c3ffd9e67d423491311602cfc
Robert Utter <utter.robert@gmail.com>
2014-01-29 13:07:33 -0800

    docs for ChaseCam

--- Project/Assets/Plugins/ChicoPlugins/TweenCam/ChaseCam.cs
+++ Project/Assets/Plugins/ChicoPlugins/TweenCam/ChaseCam.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
 
+/// <summary>
+/// TweenCam that follows a target Transform.
+/// </summary>
+[RequireComponent(typeof(Camera))]
 public class ChaseCam : TweenCam {
+    /// <summary>
+    /// Which Transform do we chase?
+    /// </summary>
+    /// <remarks>
+    /// Can populate from inspector, or we'll try to pick on in OnStart.
+    /// </remarks>
     public Transform target;
     
     private Transform lastTarget;
     private TweenState tween;
     
+    /// <summary>
+    /// While true, SetState state so that CamState pos/target vectors are in 
+    /// target's local space.
+    /// </summary>
     protected bool applyChaseOffset = true;
     
     protected override void OnStart() {
@@ -21,6 +35,9 @@ public class ChaseCam : TweenCam {
         }
     }
     
+    /// <summary>
+    /// Modifies state so that pos/target vectors are in target's local space.
+    /// </summary>
     protected override void SetState(CamState state) {
         Vector3 offset;
         if (tween.Tick(GetDeltaTime())) {
@@ -36,11 +53,20 @@ public class ChaseCam : TweenCam {
         base.SetState(state);
     }
     
+    /// <summary>
+    /// Tweens us toward another transform over time.
+    /// </summary>
+    /// <param name="newTarget">Transform to chase.</param>
+    /// <param name="duration">Tween duration.</param>
     public void SwitchTarget(Transform newTarget, float duration=1f) {
         SnapTarget(newTarget);
         tween.Reset(duration);
     }
     
+    /// <summary>
+    /// Snaps us to another transform immediately.
+    /// </summary>
+    /// <param name="newTarget">Transform to chase.</param>    
     public void SnapTarget(Transform newTarget) {
         lastTarget = target;
         target = newTarget;
