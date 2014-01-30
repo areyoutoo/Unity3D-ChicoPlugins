b41de70c05c944bc49b2f378cad8e220ab8c7a9d
Robert Utter <utter.robert@gmail.com>
2014-01-29 18:50:00 -0800

    stupid syntax error

--- Project/Assets/Plugins/ChicoPlugins/Loading/LevelLoader.cs
+++ Project/Assets/Plugins/ChicoPlugins/Loading/LevelLoader.cs
@@ -5,7 +5,7 @@ public class LevelLoader : MonoBehaviour {
     public static void Load(string target, string transition="Loading") {
         GameObject go = new GameObject("LevelLoader");
         LevelLoader instance = go.AddComponent<LevelLoader>();
-        instance.StartCoroutine(instance.InnerLoad(target));
+        instance.StartCoroutine(instance.InnerLoad(target, transition));
     }
  
     IEnumerator InnerLoad(string target, string transition) {
