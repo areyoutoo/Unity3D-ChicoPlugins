4390b11dbb26c8bd7370abb44c02adcb1988e512
Robert Utter <utter.robert@gmail.com>
2014-02-20 15:32:26 -0800

    MusicPlayer, MusicLooper

---
+++ Project/Assets/Plugins/ChicoPlugins/Audio/MusicLooper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// MusicPlayer that re-starts its music once finished.
+/// </summary>
+/// <remarks>
+/// Still calls onMusicFinished() after re-starting the music.
+/// </remarks>
+public class MusicLooper : MusicPlayer {
+	protected override void MusicFinished() {
+		music.Play();
+		base.MusicFinished();
+	}
+}
